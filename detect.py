f54b632f66a252491d43ab83b97bbe908032d741
spylex <alex@fignet.eu>
2013-06-23 12:37:02 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(12, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.add_event_detect(11, GPIO.BOTH)
 def my_callback():
     GPIO.output(12, GPIO.input(11))
 GPIO.add_event_callback(11, my_callback)
+GPIO.cleanup()
