fc0a50eebe524e1683a9de263e3b87b3ffc72538
spylex <alex@fignet.eu>
2013-06-23 13:48:38 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,36 +1,45 @@
 from time import sleep  # Allows us to call the sleep function to slow down our loop
 import RPi.GPIO as GPIO # Allows us to call our GPIO pins and names it just GPIO
  
-GPIO.setmode(GPIO.BOARD)  # Set's GPIO pins to BCM GPIO numbering
-BUTTON_1 = 15           # Sets our input pins
-BUTTON_2 = 16           # Sets our input pins
-BUTTON_3 = 11           # Sets our input pins
+GPIO.setmode(GPIO.BOARD)  # Set's GPIO pins to BOARD GPIO numbering
+# Define component pins
+BUTTON1 = 15
+BUTTON2 = 16
+BUTTON3 = 11
 LED = 12
-GPIO.setup(BUTTON_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set our input pin to be an input, with internal pullup resistor on
-GPIO.setup(BUTTON_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set our input pin to be an input, with internal pullup resistor on
-GPIO.setup(BUTTON_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set our input pin to be an input, with internal pullup resistor on
+# Start Counts
+B1P = 0
+B2P = 0
+B3P = 0
+# Set our input pins to be an input, with internal pullup resistor on
+GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Set our output pin to be an output
 GPIO.setup(LED, GPIO.OUT)
 
 # Create functions to run when the buttons are pressed
-def Input_1(channel):
+def B1A(channel):
+    GPIO.output(LED, True);
+    B1P+1;
+    print 'B1P';
+    sleep(2);
+    GPIO.output(LED, False);
+    
+def B2A(channel):
     GPIO.output(LED, True);
     sleep(2);
     GPIO.output(LED, False);
     
-def Input_2(channel):
-    GPIO.output(LED, True);
-    sleep(2);
-    GPIO.output(LED, False);
-    
-def Input_3(channel):
+def B3A(channel):
     GPIO.output(LED, True);
     sleep(2);
     GPIO.output(LED, False);
 
 # Wait for Button 1 to be pressed, run the function in "callback" when it does, also software debounce for 300 ms to avoid triggering it multiple times a second
-GPIO.add_event_detect(BUTTON_1, GPIO.BOTH, callback=Input_1, bouncetime=300) 
-GPIO.add_event_detect(BUTTON_2, GPIO.BOTH, callback=Input_2, bouncetime=300) # Wait for Button 2 to be pressed
-GPIO.add_event_detect(BUTTON_3, GPIO.BOTH, callback=Input_3, bouncetime=300) # Wait for Button 3 to be pressed
+GPIO.add_event_detect(BUTTON1, GPIO.BOTH, callback=B1A, bouncetime=300) 
+GPIO.add_event_detect(BUTTON2, GPIO.BOTH, callback=B2A, bouncetime=300)
+GPIO.add_event_detect(BUTTON3, GPIO.BOTH, callback=B3A, bouncetime=300)
 
 # Start a loop that never ends
 while True:
