a54aaf6f0c96b9fd1ba19d0060237189305f2580
spylex <alex@fignet.eu>
2013-06-23 13:13:02 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,25 +2,30 @@ from time import sleep  # Allows us to call the sleep function to slow down our 
 import RPi.GPIO as GPIO # Allows us to call our GPIO pins and names it just GPIO
  
 GPIO.setmode(GPIO.BOARD)  # Set's GPIO pins to BCM GPIO numbering
-BUTTON_1 = 11           # Sets our input pins
-BUTTON_2 = 15           # Sets our input pins
-BUTTON_3 = 16           # Sets our input pins
+BUTTON_1 = 15           # Sets our input pins
+BUTTON_2 = 16           # Sets our input pins
+BUTTON_3 = 11           # Sets our input pins
+LED = 12
 GPIO.setup(BUTTON_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set our input pin to be an input, with internal pullup resistor on
 GPIO.setup(BUTTON_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set our input pin to be an input, with internal pullup resistor on
 GPIO.setup(BUTTON_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set our input pin to be an input, with internal pullup resistor on
+GPIO.setup(LED, GPIO.OUT)
 
 # Create functions to run when the buttons are pressed
 def Input_1(channel):
     # Put whatever Button 1 does in here
-    print 'Button 1';
+    print 'Button 1'
+    GPIO.output(LED, True);
     
 def Input_2(channel):
     # Put whatever Button 2 does in here
-    print 'Button 2';
+    print 'Button 2'
+    GPIO.output(LED, False);
     
 def Input_3(channel):
     # Put whatever Button 3 does in here
-    print 'Button 3';
+    print 'Button 3'
+    GPIO.cleanup();
 
 # Wait for Button 1 to be pressed, run the function in "callback" when it does, also software debounce for 300 ms to avoid triggering it multiple times a second
 GPIO.add_event_detect(BUTTON_1, GPIO.BOTH, callback=Input_1, bouncetime=300) 
