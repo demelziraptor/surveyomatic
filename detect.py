0fe795f369d3b0ce07a558c80c88efef7e5f345b
spylex <alex@fignet.eu>
2013-10-10 16:26:05 +0100

    Update detect.py

--- detect.py
+++ detect.py
@@ -17,8 +17,7 @@ GPIO.output(LED, True)
 
 # Create functions to run when the buttons are pressed
 def B1A(channel):
-    print (int(GPIO.input(BUTTON2)));
-    if ( int(GPIO.input(BUTTON2)) or int(GPIO.input(BUTTON3)) ):
+    if ( int(GPIO.input(BUTTON2)) = 0 or int(GPIO.input(BUTTON3)) = 0 ):
      return;
     else:
      GPIO.output(LED, False);
