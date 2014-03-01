c5f2f4c2d8d8453e3c302a546a0b46ed2ad95b5b
Demelziraptor <demelza@circularvale.com>
2014-03-01 20:21:00 -0300

    adding logging and GPIO cleanup

--- detect.py
+++ detect.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from datetime import datetime
 
 
 # Define component pins
@@ -7,6 +8,9 @@ BUTTON1 = 15
 BUTTON2 = 11
 BUTTON3 = 16
 LED = 12
+
+# Logging control
+LOGGING = True
     
     
 class Main():
@@ -31,20 +35,38 @@ class Main():
             GPIO.add_event_detect(button, GPIO.FALLING, callback=self.handle_button_press, bouncetime=5000)
         
     def handle_button_press(self, channel):
-        print_button_states()
+        self.print_button_states()
         if not GPIO.input(LED):
             return
-        GPIO.output(LED, False) 
-        print "You pressed the {colour} button".format(colour = self.button_names[channel])
+        GPIO.output(LED, False)
+        button_name = self.button_names[channel]
+        self.log_button_press(button_name)
+        print "You pressed the {bn} button".format(bn = button_name)
         sleep(10)
         GPIO.output(LED, True)
          
     def print_button_states(self):
         for button in self.buttons:
             print int(GPIO.input(button))
+            
+    def log_button_press(self, button_name):
+        if not LOGGING:
+            return
+        logfile = "{y}_week-{w}.log".format(y = datetime.now().strftime('%Y'), w = datetime.now().isocalendar()[1])
+        text = "The {bn} button was pressed at {t}".format(bn = button_name, t = datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        try:
+            with open(logfile, 'a') as f:
+                f.write(text + '\n')
+        except:
+            print "Could not open log file for writing"
 
 
 if __name__ == '__main__':
-    Main()
-    while True:
-        sleep(60)
+    try:
+        Main()
+        while True:
+            sleep(60)
+    except:
+        pass
+    finally:
+        GPIO.cleanup()
