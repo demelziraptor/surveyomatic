99f6d3de271a78b68aacc353390650019e5004f5
Demelziraptor <demelza@circularvale.com>
2014-03-25 17:32:27 -0300

    tidied email script

--- send_email.py
+++ send_email.py
@@ -5,32 +5,37 @@ from datetime import datetime
 
 PROGRAMLOG = '/var/log/surveyomatic.log'
 
-SUBJECT = 'Surveyomatic Weekly Log File'
 FROM = 'pi@lucidica.com'
 TO = 'demelza.buckham@lucidica.com'
 LOGFILE = "logs/{y}_week-{w}.log".format(y = datetime.now().strftime('%Y'), w = datetime.now().isocalendar()[1]-1)
 
 
+def get_log_contents():
+    try:
+        with open(LOGFILE, 'r') as f:
+            msg = MIMEText(f.read())
+    except:
+        with open(PROGRAMLOG, 'a') as f:
+            f.write('Could not open log file for emailing' + '\n')
+        exit(0)
+    return msg
+    
+
 class SendEmail():
 
-    def __init__(subject=SUBJECT,msg='',sender=FROM,to=TO):
-        if msg == 'SENDLOG':
-            try:
-                with open(LOGFILE, 'r') as f:
-                    msg = MIMEText(f.read())
-            except:
-                with open(PROGRAMLOG, 'a') as f:
-                    f.write('Could not open log file for emailing' + '\n')
-                exit(0)
-        else:
-            msg = MIMEText(msg)
+    def __init__(self, subject='Surveyomatic email',contents='',sender=FROM,recipient=TO):
+        
+        msg = MIMEText(contents)
         msg['Subject'] = subject
         msg['From'] = sender
-        msg['To'] = to
+        msg['To'] = recipient
 
         s = smtplib.SMTP('10.0.30.99')
+        print str(msg)
+        exit(0)
         s.sendmail(sender, to, msg.as_string())
         s.quit()
 
+
 if __name__ == '__main__':
-    SendEmail(msg='SENDLOG')
+    SendEmail('Surveyomatic Weekly Log File',get_log_contents())
